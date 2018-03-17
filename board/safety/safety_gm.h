6664d72d863f5d11e28c6f34d73584ab7a3204f4
Martin Renschler <m.r@mail.com>
2018-03-16 22:49:04 -0700

    ASCM detection needs to be there

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -43,11 +43,8 @@ static void gm_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   // check if stock ASCM ECU is still online
   int bus_number = (to_push->RDTR >> 4) & 0xFF;
   if (bus_number == 0 && addr == 715) {
-//    gm_ascm_detected = 1;
-//    controls_allowed = 0;
-#ifdef DEBUG
-    puts("ASCM ECU still online, controls_allowed=0\n");
-#endif
+    gm_ascm_detected = 1;
+    controls_allowed = 0;
   }
 
   // ACC steering wheel buttons
